ec6730b9069759f1da55c666d0f8dc0ed4fd9f93
egorburahek <burachekgogo@gmail.com>
2025-07-28 18:39:56 +0300

    comment

---
+++ main.js
@@ -0,0 +1,35 @@
+ function changeImage(img) {
+      img.src = "foto/conandoil.webp"; // путь к изображению при наведении
+    }
+
+    function resetImage(img) {
+      img.src = "foto/detektiv.webp"; // оригинальное изображение
+    }
+
+    
+ function changeImage(img) {
+      img.src = "foto/levis.jpg"; // путь к изображению при наведении
+    }
+
+    function resetImage(img) {
+      img.src = "foto/fentezi.jpg"; // оригинальное изображение
+    }
+
+
+     function changeImage(img) {
+      img.src = "foto/shevchenko-taras-hryhorovych"; // путь к изображению при наведении
+    }
+
+    function resetImage(img) {
+      img.src = "foto/ukrclasika.webp"; // оригинальное изображение
+    }
+
+
+      function changeImage(img) {
+      img.src = "foto/serstiv.webr"; // путь к изображению при наведении
+    }
+
+    function resetImage(img) {
+      img.src = "foto/agata.webp"; // оригинальное изображение
+    }
+    
